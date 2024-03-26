11d483580453e2f9a50844ba3dd8d9608199976f
Sunbin-Lee <gozjd1234@naver.com>
2024-03-27 01:11:01 +0900

    update experimental setting

--- run_xgb.bat
+++ run_xgb.bat
@@ -1,7 +1,7 @@
 for %%n in (100, 200, 500) do (
     for %%d in (3, 4, 5, 6) do (
-        for %%l in (0.2, 0.1, 0.05) do (
-            for %%m in (30, 100) do (
+        for %%l in (0.1, 0.05) do (
+            for %%m in (30, 50, 100) do (
                 python main_xgb.py --learning_rate %%l --n_estimators %%n --max_depth %%d --min_child_weight %%m
             )
         )
