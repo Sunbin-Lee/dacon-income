ccffb46122c4290632e3442cf14dfc2c0ef44bd8
Sunbin-Lee <gozjd1234@naver.com>
2024-03-26 20:54:50 +0900

    update preprocessing

--- data.py
+++ data.py
@@ -14,27 +14,82 @@ from utils import *
 train = pd.read_csv('data/train.csv')
 test = pd.read_csv('data/test.csv')
 
+
+## drop duplicated
+features = ['Age', 'Gender', 'Education_Status', 'Employment_Status',
+    'Working_Week (Yearly)', 'Industry_Status', 'Occupation_Status', 'Race',
+    'Hispanic_Origin', 'Martial_Status', 'Household_Status',
+    'Household_Summary', 'Citizenship', 'Birth_Country',
+    'Birth_Country (Father)', 'Birth_Country (Mother)', 'Tax_Status',
+    'Gains', 'Losses', 'Dividends', 'Income_Status',
+    'Income' # Income 확인 여부
+    ]
+
+train.drop_duplicates(features, inplace=True)
+train.reset_index(drop=True, inplace=True)
+
+print('drop duplicates')
+print(len(train))
+
 train['Education'] = train['Education_Status']
 train['Education'] = train['Education'].apply(modify_education)
 test['Education'] = test['Education_Status']
 test['Education'] = test['Education'].apply(modify_education)
 
-train['Household_Summary'] = train['Household_Summary'].apply(modify_household)
-test['Household_Summary'] = test['Household_Summary'].apply(modify_household)
+# train['Household_Summary'] = train['Household_Summary'].apply(modify_household)
+# test['Household_Summary'] = test['Household_Summary'].apply(modify_household)
 
-train['Tax_Status'] = train['Tax_Status'].apply(modify_tax)
-test['Tax_Status'] = test['Tax_Status'].apply(modify_tax)
+# train['Tax_Status'] = train['Tax_Status'].apply(modify_tax)
+# test['Tax_Status'] = test['Tax_Status'].apply(modify_tax)
 
 train['Income_Status'] = train['Income_Status'].apply(modify_income_status)
 test['Income_Status'] = test['Income_Status'].apply(modify_income_status)
 
-train['Working_Week (Yearly)'] = np.where(train['Working_Week (Yearly)']<52, 0, 1)
-test['Working_Week (Yearly)'] = np.where(test['Working_Week (Yearly)']<52, 0, 1)
+### outlier 제거
+under = train.loc[train['Income_Status']=='Under Median']
 
-train['Capital'] = train['Gains'] + train['Dividends'] - train['Losses']
-test['Capital'] = test['Gains'] + test['Dividends'] - test['Losses']
-train['Capital'] = np.where(train['Capital']>0, 1, 0)
-test['Capital'] = np.where(test['Capital']>0, 1, 0)
+Q1 = np.quantile(under['Income'], 0.25)
+Q3 = np.quantile(under['Income'], 0.75)
+IOR = Q3-Q1
+outlier_step = IOR*1.5
+
+u_outliers = under.loc[(under['Income']<Q1-outlier_step)|(under['Income']>Q3+outlier_step)]
+len(u_outliers)
+
+over = train.loc[train['Income_Status']=='Over Median']
+
+Q1 = np.quantile(over['Income'], 0.25)
+Q3 = np.quantile(over['Income'], 0.75)
+IOR = Q3-Q1
+outlier_step = IOR*1.5
+
+o_outliers = over.loc[(over['Income']<Q1-outlier_step)|(over['Income']>Q3+outlier_step)]
+len(o_outliers)
+
+outliers = list(u_outliers.index) + list(o_outliers.index)
+
+train.drop(index=outliers, inplace=True)
+train.reset_index(drop=True, inplace=True)
+
+print('drop outliers')
+print(len(train))
+
+# train['Working_Week (Yearly)'] = np.where(train['Working_Week (Yearly)']<52, 0, 1)
+# test['Working_Week (Yearly)'] = np.where(test['Working_Week (Yearly)']<52, 0, 1)
+
+train['Gains'] = np.where(train['Gains']>0, 1, 0)
+test['Gains'] = np.where(test['Gains']>0, 1, 0)
+
+train['Losses'] = np.where(train['Losses']>0, 1, 0)
+test['Losses'] = np.where(test['Losses']>0, 1, 0)
+
+train['Dividends'] = np.where(train['Dividends']>0, 1, 0)
+test['Dividends'] = np.where(test['Dividends']>0, 1, 0)
+
+# train['Capital'] = train['Gains'] + train['Dividends'] - train['Losses']
+# test['Capital'] = test['Gains'] + test['Dividends'] - test['Losses']
+# train['Capital'] = np.where(train['Capital']>0, 1, 0)
+# test['Capital'] = np.where(test['Capital']>0, 1, 0)
 
 trainval_x = train.drop(columns=['ID', 'Income', 
                                 #  'Industry_Status',
@@ -43,7 +98,7 @@ trainval_x = train.drop(columns=['ID', 'Income',
                                  'Birth_Country', 
                                  'Birth_Country (Father)', 'Birth_Country (Mother)',
                                  'Education_Status',
-                                 'Gains', 'Losses', 'Dividends'
+                                #  'Gains', 'Losses', 'Dividends'
                                  ])
 
 trainval_y = train['Income']
@@ -54,11 +109,14 @@ test_x = test.drop(columns=['ID',
                             'Birth_Country', 
                             'Birth_Country (Father)', 'Birth_Country (Mother)',
                             'Education_Status',
-                            'Gains', 'Losses', 'Dividends'
+                            # 'Gains', 'Losses', 'Dividends'
                             ])
 
 # income_cat = train['Income'].apply(income_cat)
-income_over = train['Income'] > 875
+income_over = train['Income'] > 900
+
+print(trainval_x.columns)
+print(len(trainval_x.columns))
 
 encoding_target = list(trainval_x.dtypes[trainval_x.dtypes == "object"].index)
 
@@ -84,7 +142,7 @@ for i in encoding_target:
         print(i)
         continue
 
-    ohe = OneHotEncoder(sparse_output=False, handle_unknown='ignore') ## ignore로 학습 데이터셋에 없는 데이터가 들어와도 무시
+    ohe = OneHotEncoder(sparse=False, handle_unknown='ignore') ## ignore로 학습 데이터셋에 없는 데이터가 들어와도 무시
     train_encoded = ohe.fit_transform(trainval_x[[i]])
 
     train_encoded_df = pd.DataFrame(train_encoded, columns=[f"{i}_{j}" for j in range(train_encoded.shape[1])])
@@ -96,5 +154,5 @@ for i in encoding_target:
     test_x = pd.concat([test_x.drop(columns=[i]), test_encoded_df], axis=1)
 
 
-with open('data_1.pkl', 'wb') as f:
+with open('data.pkl', 'wb') as f:
     pickle.dump([trainval_x, trainval_y, test_x, income_over], f)
