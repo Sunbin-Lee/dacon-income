1ea9b436129979713b706d26858a7430a2ef9ea0
Sunbin-Lee <gozjd1234@naver.com>
2024-03-27 21:08:33 +0900

    update experimental setting

--- run_gbr.bat
+++ run_gbr.bat
@@ -1,8 +1,8 @@
-for %%l in (0.2, 0.1, 0.05) do (
+for %%l in (0.1, 0.05) do (
     for %%n in (100, 200, 500) do (
         for %%d in (3, 4, 5) do (
-            for %%s in (6, 8, 10) do (
-                for %%f in (6, 8, 10) do (
+            for %%s in (8, 16, 30) do (
+                for %%f in (16, 30) do (
                     python main_gbr.py --learning_rate %%l --n_estimators %%n --max_depth %%d --min_samples_split %%s --min_samples_leaf %%f
                 )
             )
