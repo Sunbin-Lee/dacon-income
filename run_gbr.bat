078945eaf6f2d4d86aa50027c520010545f1daba
Sunbin-Lee <gozjd1234@naver.com>
2024-03-31 18:47:22 +0900

    add echo off

--- run_gbr.bat
+++ run_gbr.bat
@@ -1,8 +1,10 @@
-for %%l in (0.1, 0.05) do (
-    for %%n in (100, 200, 500) do (
-        for %%d in (3, 4, 5) do (
-            for %%s in (8, 16, 30) do (
-                for %%f in (16, 30) do (
+@echo off
+
+for %%l in (0.1 0.05) do (
+    for %%n in (100 200 500) do (
+        for %%d in (3 4 5) do (
+            for %%s in (8 16 30) do (
+                for %%f in (16 30) do (
                     python main_gbr.py --learning_rate %%l --n_estimators %%n --max_depth %%d --min_samples_split %%s --min_samples_leaf %%f
                 )
             )
--- run_mlp.bat
+++ run_mlp.bat
@@ -1,8 +1,13 @@
-for %%h in (100, 200) do (
-    for %%a in (1e-4, 1e-5, 1e-6) do (
-        for %%i in (200, 500) do (
-            python main_mlp.py --hidden_layer_sizes %%h --alpha %%a --max_iter %%i
+@echo off
+
+for %%r in (constant invscaling adaptive) do (
+    for %%h in (50 100 200) do (
+        for %%l in (1e-3, 1e-4 1e-5) do (
+            for %%a in (1e-6, 1e-7, 1e-8) do (
+                for %%i in (50, 100, 200, 500, 1000) do (
+                    python main_mlp.py --learning_rate %%r --hidden_layer_sizes %%h --learning_rate_init %%l --alpha %%a --max_iter %%i
+                )
+            )
         )
     )
-
 )
